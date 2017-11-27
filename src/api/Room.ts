ff53e8b183897164ae8d7dbb60cda629cc92d1c1
Sygeman <sygeman@gmail.com>
2017-11-27 17:36:21 +0300

    mock fix

--- src/api/Room.ts
+++ src/api/Room.ts
@@ -5,18 +5,18 @@ const testRoom = {
   avatar: 'https://pp.userapi.com/c626221/v626221510/7026b/zKYk5tlr530.jpg',
   mode: null,
   followerMode: false,
-  slowMode: false,
+  slowMode: true,
   followersCount: 2140124,
-  connectionsCount: 1593,
-  usersCount: 1103,
-  guestsCount: 490
+  connectionsCount: 41593,
+  usersCount: 41000,
+  guestsCount: 593
 };
 
 const testRoom2 = {
   id: 2,
-  name: 'test',
-  title: 'Test',
-  avatar: 'https://pp.userapi.com/c626221/v626221510/7026b/zKYk5tlr530.jpg',
+  name: 'neuropunk',
+  title: 'Neuropunk',
+  avatar: 'https://pp.userapi.com/c636226/v636226961/3728d/ghYiDWcd7fQ.jpg',
   mode: null,
   followerMode: false,
   slowMode: false,
