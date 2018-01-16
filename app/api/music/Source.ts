823834d83d53a882e2e846445125e21de6138b01
Sygeman <sygeman@gmail.com>
2018-01-16 16:18:08 +0300

    cache off

--- app/api/music/Source.ts
+++ app/api/music/Source.ts
@@ -1,90 +1,89 @@
-import { getConnection } from "typeorm";
+import { getConnection } from 'typeorm';
 import { Source as SourceEntity } from 'app/entity/Source';
 import { youtubeAPI, soundcloudAPI } from 'app/api';
 
 export class SourceAPI {
+	get repository() {
+		return getConnection().getRepository(SourceEntity);
+	}
 
-  get repository() {
-    return getConnection().getRepository(SourceEntity);
-  }
+	get manager() {
+		return getConnection().manager;
+	}
 
-  get manager() {
-    return getConnection().manager;
-  }
+	addFromLink = async (link: string) => {
+		const ytRegex = /((www\.|)youtube\.com|youtu\.?be)\/(watch\?v=|v|embed\/|)([^\?\&\s]+)/;
+		const scRegex = /(https?:\/\/soundcloud.com\/.+)/;
 
-  addFromLink = async (link: string) => {
-    const ytRegex = /((www\.|)youtube\.com|youtu\.?be)\/(watch\?v=|v|embed\/|)([^\?\&\s]+)/;
-    const scRegex = /(https?:\/\/soundcloud.com\/.+)/;
+		const ytRegexRes = link.match(ytRegex);
 
-    const ytRegexRes = link.match(ytRegex);
+		if (ytRegexRes && ytRegexRes[4]) {
+			return this.addFromYoutubeById(ytRegexRes[4]);
+		}
 
-    if (ytRegexRes && ytRegexRes[4]) {
-      return this.addFromYoutubeById(ytRegexRes[4]);
-    }
+		const scRegexRes = link.match(scRegex);
 
-    const scRegexRes = link.match(scRegex);
+		if (scRegexRes) {
+			return this.addFromSoundcloudByUrl(scRegexRes[0]);
+		}
 
-    if (scRegexRes) {
-      return this.addFromSoundcloudByUrl(scRegexRes[0]);
-    }
+		return false;
+	};
 
-    return false;
-  }
+	getOne = async (where) => {
+		return this.repository.findOne({ where });
+	};
 
-  getOne = async (where) => {
-    return this.repository.findOne({ where, cache: true });
-  }
+	getById = async (sourceId) => {
+		return this.getOne({ id: sourceId });
+	};
 
-  getById = async (sourceId) => {
-    return this.getOne({ id: sourceId });
-  }
+	save = async (data) => {
+		let source = new SourceEntity();
 
-  save = async (data) => {
-    let source = new SourceEntity();
+		for (const name of Object.keys(data)) {
+			source[name] = data[name];
+		}
 
-    for (const name of Object.keys(data)) {
-      source[name] = data[name];
-    }
+		return this.manager.save(source);
+	};
 
-    return this.manager.save(source);
-  }
+	addFromYoutubeById = async (videoId: string) => {
+		const source = await this.getOne({
+			service: 'youtube',
+			serviceId: videoId
+		});
 
-  addFromYoutubeById = async (videoId: string) => {
-    const source = await this.getOne({
-      service: 'youtube',
-      serviceId: videoId
-    });
+		if (source) {
+			return source;
+		}
 
-    if (source) {
-      return source;
-    }
+		const data = await youtubeAPI.getTrackById(videoId);
 
-    const data = await youtubeAPI.getTrackById(videoId);
+		if (!data) {
+			return false;
+		}
 
-    if (!data) {
-      return false;
-    }
+		const newSource = await this.save(data);
 
-    const newSource = await this.save(data);
+		return newSource;
+	};
 
-    return newSource;
-  }
+	addFromSoundcloudByUrl = async (url: string) => {
+		const source = await this.getOne({ service: 'soundcloud', url });
 
-  addFromSoundcloudByUrl = async (url: string) => {
-    const source = await this.getOne({ service: 'soundcloud', url });
+		if (source) {
+			return source;
+		}
 
-    if (source) {
-      return source;
-    }
+		const data = await soundcloudAPI.getTrackByUrl(url);
 
-    const data = await soundcloudAPI.getTrackByUrl(url);
+		if (!data) {
+			return false;
+		}
 
-    if (!data) {
-      return false;
-    }
+		const newSource = await this.save(data);
 
-    const newSource = await this.save(data);
-
-    return newSource;
-  }
+		return newSource;
+	};
 }
