24d4ed502475b6abd9482ab236e2a678fd8b7f05
Sygeman <sygeman@gmail.com>
2018-01-26 02:39:23 +0300

    move chat methods to mutations

--- app/wsapi/index.ts
+++ app/wsapi/index.ts
@@ -1,26 +1,23 @@
-export * from './chat';
-
 import { redis } from 'core/db';
-import { pubSub } from 'core/pubsub';
 import { logger } from 'core/logger';
-import { connectionAPI, connectionEventsAPI, roomEventsAPI } from 'app/api';
+import { connectionEventsAPI, roomEventsAPI } from 'app/api';
 const { AUTH_KEY_SECRET } = process.env;
 
 export async function connect(cdata) {
 	const { connectionId } = cdata;
-	// logger.info('connect', connectionId);
-
 	connectionEventsAPI.onJoin(connectionId);
 }
 
 export async function disconnect(cdata) {
 	const { connectionId } = cdata;
-	// logger.info('disconnect', connectionId);
-
 	connectionEventsAPI.onLeave(connectionId);
 }
 
 export async function login(token: string, cdata) {
+	if (cdata.token === token) {
+		return false;
+	}
+
 	const tokenData = await redis.get(`connectionToken:${token}`);
 
 	if (!tokenData) {
@@ -30,32 +27,28 @@ export async function login(token: string, cdata) {
 
 	redis.del(`connectionToken:${token}`);
 
+	cdata.token = token;
 	const userId = parseInt(tokenData, 10);
-	// logger.info('login', userId);
 	cdata.userId = userId;
-
 	connectionEventsAPI.onLogin(cdata.connectionId, userId);
 }
 
-export async function logout(token: string, cdata) {
-	const userId = cdata.userId;
-	// logger.info('logout', userId);
-	cdata.userId = null;
-
-	connectionEventsAPI.onLogout(cdata.connectionId);
-}
-
 export async function join(roomId: number, cdata) {
-	// logger.info('join room', roomId);
-	cdata.roomId = roomId;
+	if (cdata.roomId == roomId) {
+		return false;
+	}
 
+	cdata.roomId = roomId;
 	roomEventsAPI.onJoin(roomId, cdata.connectionId);
 }
 
 export async function leave(data: any, cdata) {
 	const roomId = cdata.roomId;
-	// logger.info('leave room', roomId);
-	cdata.roomId = null;
 
+	if (!roomId) {
+		return false;
+	}
+
+	cdata.roomId = null;
 	roomEventsAPI.onLeave(roomId, cdata.connectionId);
 }
