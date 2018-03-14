5d9e6b5a0c69446dee43f72d5023c8600fbe7972
Sygeman <sygeman@gmail.com>
2018-03-14 21:31:17 +0300

    ms wip

---
+++ app/api/connection/ConnectionEvents.ts
@@ -0,0 +1,64 @@
+import { logger } from 'core/logger';
+import { connectionAPI, roomEventsAPI } from 'app/api';
+
+export class ConnectionEventsAPI {
+
+  async onJoin(connectionId: string, instanceId: string) {
+    return connectionAPI.save(connectionId, instanceId);
+  }
+
+  async onLeave(connectionId: string) {
+    let connection = await connectionAPI.getOne(connectionId);
+
+    if (!connection) {
+      logger.error('onLeave Error');
+      return false;
+    }
+
+    const { roomId } = connection;
+
+    if (roomId) {
+      await roomEventsAPI.onLeave(roomId, connectionId);
+    }
+    
+    return connectionAPI.del(connectionId);
+  }
+
+  async onLogin(connectionId: string, userId: number) {
+    await connectionAPI.setUserId(connectionId, userId);
+  
+    let connection = await connectionAPI.getOne(connectionId);
+    
+    if (!connection) {
+      logger.error('onLogin Error');
+      return false;
+    }
+
+    const { roomId } = connection;
+  
+    if (roomId) {
+      await roomEventsAPI.onLogin(roomId, connectionId, userId);
+    }
+  
+    return true;
+  }
+
+  async onLogout(connectionId: string) {
+    let connection = await connectionAPI.getOne(connectionId);
+    
+    if (!connection) {
+      logger.error('onLogout Error');
+      return false;
+    }
+
+    const { roomId, userId } = connection;
+
+    await connectionAPI.setUserId(connectionId, null);
+    
+    if (roomId) {
+      await roomEventsAPI.onLogout(roomId, connectionId, userId);
+    }
+  
+    return true;
+  }
+}
--- core/pubsub.ts
+++ core/pubsub.ts
@@ -1,44 +1,13 @@
-import { setupRedis } from 'core/db';
-import { logger } from 'core/logger';
+import { broker } from 'core/broker';
 
 class PubSub {
-  pub: any;
-  sub: any;
-
-  constructor() {
-    this.pub = setupRedis();
-    this.sub = setupRedis();
-
-    this.setup();
-  }
-
-  private setup() {
-    this.sub.subscribe('pubsub', (err, count) => {
-      if (err) {
-        logger.error(err);
-      }
-    });
-    
-    this.sub.on('message', (channel, message) => {
-      if (channel === 'pubsub') {
-        this.onMessage(message);
-      }
-    });
-  }
-
-  private onMessage(message) {
-    // const { eventName, payload, filterData } = JSON.parse(message);
-  }
 
   public publish(eventName, payload, filterData) {
-    const mPayload = {
-      eventName,
-      payload,
-      filterData
-    };
-
-    this.pub.publish('pubsub', JSON.stringify(mPayload));
+    broker.broadcast("wsapi.publish", {
+      eventName, payload, filterData
+    });
   }
+
 }
 
 export const pubSub = new PubSub();
