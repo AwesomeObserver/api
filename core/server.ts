34f28815b5e05dc85a0ec632cdf9ae98af2456e7
Sygeman <sygeman@gmail.com>
2018-04-20 17:06:16 +0300

    cors fix

--- core/server.ts
+++ core/server.ts
@@ -84,7 +84,12 @@ export class RPServer {
 		this.router = new koaRouter();
 
 		this.app.use(koaBody());
-		this.app.use(cors());
+		this.app.use(
+			cors({
+				origin: true,
+				credentials: true
+			})
+		);
 
 		this.setupAuth();
 		this.setupGQL();
